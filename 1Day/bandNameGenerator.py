7c43431576d1d19335cb0ade062d7c4a1ffed0c5
xhoa <send2me2nd@gmail.com>
2022-04-21 23:06:44 +0200

    added bandNameGenerator.py

---
+++ 1Day/bandNameGenerator.py
@@ -0,0 +1,11 @@
+
+def bandNameGenarator():
+    run = True
+    while run:
+        print("Welcome to the Band Name Generator")
+        cityName = input("What's the name of the city you grew up in?\n")
+        petName = input("What's your pet's name?\n")
+        print(f"Your band name could be {cityName} {petName}.")
+        run = False
+
+bandNameGenarator()
