26c203bb16305f41e6ffefaf9c11046f69c8575b
xhoa <send2me2nd@gmail.com>
2022-04-28 22:28:23 +0200

    Added password generator

--- 5Day/password_generator.py
+++ 5Day/password_generator.py
@@ -12,9 +12,45 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+
+def generate_password():
+    # initialize emtpy password
+    password = ""
+
+    # 3 for loops for each
+    selected_letters = [' ' for i in range(nr_letters)]
+    for i in range(nr_letters):
+        index_letters = random.randint(0, len(letters) - 1)
+        selected_letters[i] = letters[index_letters]
+
+    selected_symbols = [' ' for i in range(nr_symbols)]
+    for i in range(nr_symbols):
+        index_symbols = random.randint(0, len(symbols) - 1)
+        selected_symbols[i] = symbols[index_symbols]
+
+    selected_numbers = [' ' for i in range(nr_numbers)]
+    for i in range(nr_numbers):
+        index_numbers = random.randint(0, len(numbers) - 1)
+        selected_numbers[i] = numbers[index_numbers]
+
+    password = selected_letters + selected_symbols + selected_numbers
+    return password
+
+
 # Eazy Level - Order not randomised:
 # e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
 
 # Hard Level - Order of characters randomised:
 # e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+def generate_password_order_randomised():
+    password_not_randomised = generate_password()
+    password_randomised = ""
+    while len(password_not_randomised) != 0:
+        char = random.choice(password_not_randomised)
+        password_randomised += char
+        password_not_randomised.remove(char)
+    return password_randomised
+
+
+print(generate_password_order_randomised())
